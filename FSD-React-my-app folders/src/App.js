5d28afc8c6558e717e1e57a97e84959580f3ea13
Beulah Daizy <137136832+beulahdaizy@users.noreply.github.com>
2023-11-03 16:09:52 +0530

    Add files via upload

---
+++ FSD-React-my-app folders/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "./Components/Main";
+import "./App.css";
+import "./Components/Style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
